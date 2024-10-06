043785fde7c5e58ea8c8a80b1730205f62e4a611
David Villalobos <villalobosgonzalezluisdavid@gmail.com>
2024-10-06 11:30:35 -0600

    Revert "Merge remote-tracking branch 'origin/alex/data_ingestion' into test-backend"
    This reverts commit 6018e04515b83ac0449217133d159664d232ce6b, reversing
    changes made to 25a99b2f0bd9ccc7b592a13f7f6670eb744568e8.

--- src/data_ingestion/main.py
+++ src/data_ingestion/main.py
@@ -1,36 +1,5 @@
-<<<<<<< HEAD
-import os
-from dotenv import load_dotenv
-from proxy import Proxy
-
-load_dotenv()
-
-NASA_PASSWORD = os.getenv('NASA_PASSWORD')
-NASA_USERNAME = os.getenv('NASA_USERNAME')
-
 def main():
-    p = Proxy(NASA_USERNAME, NASA_PASSWORD)
-    p.fetch_scenes(9.9281,-84.0907,'2024-09-01','2024-09-30',10)
-=======
-from proxy import Proxy
-def main():
-    print("Running data ingestion pipeline...")
-    username = "galac-ticos-api"
-    password = "Galacticos123!"
-
-    proxy = Proxy(username, password)
-
-    scenes = proxy.search_scenes(
-        dataset='landsat_tm_c2_l1',
-        latitude=50.85,
-        longitude=-4.35,
-        start_date='1995-01-01',
-        end_date='1995-10-01',
-        max_cloud_cover=10
-    )
-
-    proxy.download_scene(scenes[0])
->>>>>>> data_ingestion
+    print("Hello, World!")
 
 if __name__ == "__main__":
     main()

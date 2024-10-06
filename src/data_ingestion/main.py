eba305c930f697fc3c23cf533bfa01f2bd201229
mantofer02 <mantofer2000@gmail.com>
2024-10-06 01:59:11 -0600

    Refactor data ingestion module and add utility functions
    - Refactor the data_ingestion module to improve code organization and readability.
    - Add a new decompress.py file to handle decompression of tar files.
    - Create a utils.py file to contain utility functions for data processing.
    - Implement the extract_tar_files function in utils.py to extract tar files.
    - Implement the write_scene_json function in utils.py to write scene data to JSON files.

--- src/data_ingestion/main.py
+++ src/data_ingestion/main.py
@@ -7,9 +7,12 @@ load_dotenv()
 NASA_PASSWORD = os.getenv('NASA_PASSWORD')
 NASA_USERNAME = os.getenv('NASA_USERNAME')
 
+
 def main():
     p = Proxy(NASA_USERNAME, NASA_PASSWORD)
-    p.fetch_scenes(9.9281,-84.0907,'2024-08-01','2024-10-05',0,datasets=['landsat_ot_c2_l2'])
+    p.fetch_scenes(9.9281, -84.0907, '2024-08-01', '2024-08-02',
+                   0, datasets=['landsat_ot_c2_l2'])
+
 
 if __name__ == "__main__":
     main()

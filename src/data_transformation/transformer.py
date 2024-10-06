1a94feb3ad0b632fd1f0a5e44786dd09386e4e6d
mantofer02 <mantofer2000@gmail.com>
2024-10-06 17:46:47 -0600

    Refactor Transformer class to add get_scene_metadata method

--- src/data_transformation/transformer.py
+++ src/data_transformation/transformer.py
@@ -3,7 +3,23 @@ import utils
 
 class Transformer:
     def __init__(self) -> None:
-        pass
+        self.user_form = None
 
     def transform(self, scene):
         pass
+
+    def get_scene_metadata(self, data_path='../data'):
+        metadata_json = utils.find_metadata_json_files(data_path)[0]
+
+        metadata_user = {
+            "satellite": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["SPACECRAFT_ID"],
+            "sensor": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["SENSOR_ID"],
+            "date": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["DATE_ACQUIRED"],
+            "cloud_cover": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["CLOUD_COVER"],
+            "cloud_cover_land": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["CLOUD_COVER_LAND"],
+            "image_quality": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["IMAGE_QUALITY_OLI"],
+            "sun_elevation": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["SUN_ELEVATION"],
+            "earth_sun_distance": metadata_json["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["EARTH_SUN_DISTANCE"],
+        }
+
+        return metadata_user
